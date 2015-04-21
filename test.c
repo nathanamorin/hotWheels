b8b715307698118ce6c792fd8a91d4dfd545c575
Nathan Morin <nathanamorin@users.noreply.github.com>
2015-04-21 17:54:18 -0400

    Create test.c

---
+++ test.c
@@ -0,0 +1,51 @@
+#include <pthread.h>
+#include <stdio.h>
+
+void changeSpeed(void)
+{
+
+         FILE *file;
+         for(;;)
+         {
+            file = fopen("stop.txt", "r");
+            if (fgetc(file) == 's') break;
+            fclose(file);
+
+            //Do speed stuff here
+         }
+
+         //This just creates an ending file in the same directery that shows
+         //The function has successfuly been told to stop.
+         printf("ENDING\n");
+         file = fopen("itEnded.txt","a");
+         fputs("The Function Has Ended",file);
+         fclose(file);
+         pthread_exit(0);
+
+}
+
+//This is like the web handler
+int webHandler()
+{
+    pthread_t hilo1;
+
+    pthread_create(&hilo1,NULL, changeSpeed, NULL);
+
+    printf("The test example web handler function has finished");
+    return 0 ;
+
+}
+
+//This is like web server core function
+int main(void)
+{
+
+    for (;;)
+    {
+        //This is like web call
+        webHandler();
+        int input;
+        scanf("%d",&input);
+    }
+
+}
